ad84ef00533c1e1e5cec3c5deaf3ab22d7d0ddce
Pavel Mareev <mareev.pv@gmail.com>
2023-09-23 11:34:59 +0200

    feat: add manual mocks and types [module1-task2]

---
+++ src/shared/types/offer.type.ts
@@ -0,0 +1,22 @@
+import { City } from './city.type-enum.js';
+import { Feature } from './feature.type-enum.js';
+import { HousingType } from './housingType.type-enum.js';
+
+export type Offer = {
+  title: string;
+  description: string;
+  datePublished: string;
+  city: City;
+  preview: string;
+  photos: string[];
+  isPremium: boolean;
+  isFavorite: boolean;
+  rating: number;
+  housingType: HousingType;
+  roomCount: number;
+  tenantCount: number;
+  price: number;
+  features: Feature[];
+  authorId: string;
+  location: [number, number];
+}
