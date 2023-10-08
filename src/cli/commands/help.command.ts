0a3eb2d2c0f2da3a10c9a45b3b280c96c548b7df
Pavel Mareev <mareev.pv@gmail.com>
2023-10-08 20:21:23 +0200

    fix: cli-import, database uri fallback from env [module5-task1]

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -16,7 +16,7 @@ export class HelpCommand implements Command {
 
 ${chalk.cyan('--version')}:                       ${chalk.gray('# выводит номер версии')}
 ${chalk.cyan('--help')}:                          ${chalk.gray('# печатает этот текст')}
-${chalk.cyan('--import')} ${chalk.magenta('<path> <database_uri>')}:  ${chalk.gray('# импортирует данные из TSV в базу данных')}
+${chalk.cyan('--import')} ${chalk.magenta('<path> [database_uri]')}:  ${chalk.gray('# импортирует данные из TSV в базу данных')}
 ${chalk.cyan('--generate')} ${chalk.magenta('<n> <path> <url>')}:     ${chalk.gray('# генерирует произвольное количество тестовых данных')}
     `);
   }
