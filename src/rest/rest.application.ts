9618707e24081fc93199c7d4b0f9e5a2213055a3
Pavel Mareev <mareev.pv@gmail.com>
2023-10-21 17:57:44 +0200

    feat: init server [module6-task1]

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'inversify';
+import express, { Express } from 'express';
 import { RestConfigSchema, Config } from '../shared/lib/config/index.js';
 import { Logger } from '../shared/lib/logger/logger.interface.js';
 import { Component } from '../shared/types/component.enum.js';
@@ -7,14 +8,23 @@ import { getMongoUrl } from '../shared/utils/database.js';
 
 @injectable()
 export class RestApplication {
+  private server: Express;
+
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestConfigSchema>,
     @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient
-  ) {}
+  ) {
+    this.server = express();
+  }
 
-  public async init() {
-    this.logger.info(`PORT=${this.config.get('PORT')}`);
+  private async initServer() {
+    return new Promise<void>((resolve) => {
+      this.server.listen(this.config.get('PORT'), resolve);
+    });
+  }
+
+  private async initDb() {
     await this.databaseClient.connect(getMongoUrl({
       username: this.config.get('DB_USERNAME'),
       password: this.config.get('DB_PASSWORD'),
@@ -22,6 +32,17 @@ export class RestApplication {
       port: this.config.get('DB_PORT'),
       databaseName: this.config.get('DB_NAME'),
     }));
+  }
+
+  public async init() {
+    this.logger.info('Init database');
+    await this.initDb();
+    this.logger.info('Init database completed');
+
+    this.logger.info('Init server');
+    await this.initServer();
+    this.logger.info(`Server started on http://localhost:${this.config.get('PORT')}`);
+
     this.logger.info('RestApplication started');
   }
 }
